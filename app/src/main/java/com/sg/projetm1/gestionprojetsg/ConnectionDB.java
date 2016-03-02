60128d53b1b93e8a070a1b97121567b13adf7a0f
Godefroy <godefroy.29@hotmail.fr>
2016-03-02 22:31:25 +0100

    First commit Connection to database is working

---
+++ app/src/main/java/com/sg/projetm1/gestionprojetsg/ConnectionDB.java
@@ -0,0 +1,63 @@
+package com.sg.projetm1.gestionprojetsg;
+
+import android.os.AsyncTask;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ * Created by Godefroy on 02/03/2016.
+ */
+
+public class ConnectionDB extends AsyncTask<Void, Integer, Void>
+{
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... values){
+        super.onProgressUpdate(values);
+    }
+
+    @Override
+    protected Void doInBackground(Void... arg0) {
+        Connection connection;
+        Statement statement;
+        ResultSet result;
+        System.out.println("Chargement driver");
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            System.out.println("Driver Valide");
+        } catch (Exception e) {
+            System.out.println("JDBC driver fail");
+            return null;
+        }
+
+        try {
+            System.out.println("Test connection");
+            connection = DriverManager.getConnection("jdbc:mysql://192.168.1.25:3306/myBase?user=plop&password=plop");
+            System.out.println("Connection open");
+            statement = connection.createStatement();
+            System.out.println("Statement créé");
+            result = statement.executeQuery("SELECT * from plop");
+            System.out.println("SQL request done");
+            result.close();
+            statement.close();
+            connection.close();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void result) {
+
+
+    }
+}
